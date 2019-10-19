cf4e0f742edd2777d3aa5d64c503abae2b6a76f8
Kamil Nowak <info@kamilnowak.com>
2019-10-19 18:58:00 +0200

    Updates

---
+++ precache-manifest.ba5c87e9fa195c5946d633eb445d4af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769f09a04d588fdbc2367894c51a0f88",
+    "url": "/attendly/index.html"
+  },
+  {
+    "revision": "7827cacbb20e07e33fe9",
+    "url": "/attendly/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1dd152a5a64c2bbf4dd1",
+    "url": "/attendly/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7827cacbb20e07e33fe9",
+    "url": "/attendly/static/js/2.beb3116b.chunk.js"
+  },
+  {
+    "revision": "1dd152a5a64c2bbf4dd1",
+    "url": "/attendly/static/js/main.8349eb0c.chunk.js"
+  },
+  {
+    "revision": "57eb8513d637fb0b1c26",
+    "url": "/attendly/static/js/runtime-main.d0c2854b.js"
+  }
+]);
